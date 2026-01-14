29607baf0f1b1bd8c5508dccaf55189cd0e378f4
Ajayfrizzy <ajayholuwaseun@gmail.com>
2026-01-14 17:08:32 +0100

    Remove Rust build artifacts and add .gitignore

--- Rust/hello_cargo/target/debug/deps/hello_cargo.d
+++
@@ -1,5 +0,0 @@
-C:\Users\ajayh\Desktop\ckb_builder_track\Rust\hello_cargo\target\debug\deps\hello_cargo.d: src\main.rs
-
-C:\Users\ajayh\Desktop\ckb_builder_track\Rust\hello_cargo\target\debug\deps\hello_cargo.exe: src\main.rs
-
-src\main.rs:
